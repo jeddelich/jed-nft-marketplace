287d650c59f6792adc8b40ab8ff0871a8488d9fe
jeddelich@gmail.com <jeddelich@gmail.com>
2026-03-30 18:13:54 -0400

    fix: move responsiveness logic from carousel library to react to control render

--- src/components/UI/Carousel.jsx
+++ src/components/UI/Carousel.jsx
@@ -1,35 +1,63 @@
 import Slider from "react-slick";
 import { Link } from "react-router-dom";
 import { carouselSettings } from "./carouselSettings";
+import { useEffect, useRef, useState } from "react";
 
 function Carousel({ hotCollectionsData }) {
+  const sliderRef = useRef(null);
+  const [slidesToShow, setSlidesToShow] = useState(4);
+
+  useEffect(() => {
+    function updateSlides() {
+      const width = window.innerWidth;
+
+      if (width < 570) setSlidesToShow(1);
+      else if (width < 768) setSlidesToShow(2);
+      else if (width < 1200) setSlidesToShow(3);
+      else setSlidesToShow(4);
+    }
+
+    updateSlides();
+    window.addEventListener("resize", updateSlides);
+
+    return () => window.removeEventListener("resize", updateSlides);
+  }, []);
+
+  useEffect(() => {
+    if (!sliderRef.current) return;
+
+    const timeout = setTimeout(() => {
+      sliderRef.current.slickGoTo(0);
+      sliderRef.current.slickRefresh();
+    }, 100);
+
+    return () => clearTimeout(timeout);
+  }, [slidesToShow]);
 
   return (
-    <Slider {...carouselSettings}>
-      {hotCollectionsData?.map((collection) => (
+    <Slider
+      ref={sliderRef}
+      {...carouselSettings}
+      slidesToShow={slidesToShow}
+      infinite={hotCollectionsData.length > slidesToShow}
+      className="carousel-container"
+    >
+      {hotCollectionsData.map((collection) => (
         <div key={collection.id}>
           <div className="nft_coll mx-1">
             <div className="nft_wrap">
               <Link to={`/item-details/${collection.nftId}`}>
-                <img
-                  src={collection.nftImage}
-                  className="lazy img-fluid"
-                  alt=""
-                />
+                <img src={collection.nftImage} className="img-fluid" alt="" />
               </Link>
             </div>
             <div className="nft_coll_pp">
               <Link to={`/author/${collection.authorId}`}>
-                <img
-                  className="lazy pp-coll"
-                  src={collection.authorImage}
-                  alt=""
-                />
+                <img className="pp-coll" src={collection.authorImage} alt="" />
               </Link>
               <i className="fa fa-check"></i>
             </div>
             <div className="nft_coll_info">
-              <Link to="/explore">
+              <Link to={`/item-details/${collection.nftId}`}>
                 <h4>{collection.title}</h4>
               </Link>
               <span>ERC-{collection.code}</span>
--- src/components/UI/carouselSettings.js
+++ src/components/UI/carouselSettings.js
@@ -3,24 +3,4 @@ export const carouselSettings = {
   speed: 250,
   slidesToShow: 4,
   slidesToScroll: 1,
-  responsive: [
-    {
-      breakpoint: 1200,
-      settings: {
-        slidesToShow: 3,
-      },
-    },
-    {
-      breakpoint: 768,
-      settings: {
-        slidesToShow: 2,
-      },
-    },
-    {
-      breakpoint: 570,
-      settings: {
-        slidesToShow: 1,
-      },
-    },
-  ],
 };
